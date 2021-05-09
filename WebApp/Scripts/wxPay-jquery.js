71bae449a30a62d9735da34bb96336c98902bb3d
Hank <olivetchan@qq.com>
2021-05-10 04:46:48 +0800

    修复若干问题
    1.修复回调问题，支付成功、失败、取消为事件通知；
    2.修复支付金额问题；

--- WebApp/Scripts/wxPay-jquery.js
+++ WebApp/Scripts/wxPay-jquery.js
@@ -15,7 +15,7 @@
         }else{
            return false;
         }
-    };
+    };    
     $.fn.wxPay = function (signurl, openid, options, paybefore, success, fail, cancel) {
         var wx_pay = false;
         var defaults = {
@@ -36,7 +36,10 @@
             try { m += s1.split(".")[1].length } catch (e) { }
             try { m += s2.split(".")[1].length } catch (e) { }
             return Number(s1.replace(".", "")) * Number(s2.replace(".", "")) / Math.pow(10, m);
-        };        
+        }; 
+        $(document).on("success",success);
+        $(document).on("fail",fail);
+        $(document).on("cancel",cancel);
         if ($.isEmptyObject(openid)&&!$.fn.wxPay.Debug){
             $(this).click(function(){
                 alert('用户信息错误，无法调用微信支付');
@@ -69,11 +72,15 @@
                             if(typeof paybefore === 'number' && !isNaN(paybefore))
                                 fee=yuan2fen(paybefore).toFixed(0);
                             if (!(fee==0)) { //如果金额小于0，让后台决定金额，应用场景可以是例如固定或有规则金额的情况下
+                                var feeamt=0;
+                                var orderstr="";
                                 $.post(signurl, { openid: openid, tfee: fee, body: settings.desc, pid: settings.pid, param: $.fn.wxPay.OrderParam, sp_billno: $.fn.wxPay.OrderCode }, function (data) {
                                     if (data.paySign == "ERROR") {
                                         if (typeof fail === "function") fail(data.package);
                                     }
                                     else {
+                                        feeamt=data.payFee/100;
+                                        orderstr=data.mchOrder;
                                         WeixinJSBridge.invoke(
                                         'getBrandWCPayRequest', {
                                             "appId": data.appId, //公众号名称，由商户传入
@@ -84,12 +91,12 @@
                                             "paySign": data.paySign //微信签名
                                         }, function (res) {
                                             if (res.err_msg == "get_brand_wcpay_request:ok") {
-                                                if (typeof success === "function") success({fee:data.fee,ordercode:data.ordercode});
+                                                if (typeof success === "function") $(document).trigger('success', {fee:feeamt,ordercode:orderstr});
                                             } else
                                                 if (res.err_msg == "get_brand_wcpay_request:fail") {
-                                                    if (typeof fail === "function") fail(res.err_desc);
+                                                    if (typeof fail === "function") $(document).trigger('fail', res.err_desc);
                                                 } else {
-                                                    if (typeof cancel === "function") cancel(data.payNo);
+                                                    if (typeof cancel === "function") $(document).trigger('cancel', data.payNo);
                                                 }
                                         });
                                     }
