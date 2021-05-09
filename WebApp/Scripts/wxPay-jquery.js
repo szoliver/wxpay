dc46a38ae40f994cb20918e43cbd0c65f348822e
Hank <olivetchan@qq.com>
2021-05-09 12:05:48 +0800

    基于jquery3.6更新优化插件功能并修复部分问题
    1.通过函数节流方式解决重复点击多次调用支付控件的问题；
    2.优化微信浏览器版本判断；
    3.支持回调返回金额为NaN时中止支付流程；

--- WebApp/Scripts/wxPay-jquery.js
+++ WebApp/Scripts/wxPay-jquery.js
@@ -1,12 +1,11 @@
-﻿; $(function ($, window, undefined) {
+; (function ($, window) {
     'use strict';
     $.fn.wxPay = function (signurl, openid, options, paybefore, success, fail, cancel) {
         var wx_pay = false;
         var defaults = {
-            pid: "0", desc: ""
+            pid: "0", desc: "支付费用"
         };
         var settings = $.extend({}, defaults, options);
-        var abortpay = false;
         var onBridgeReady = function () { wx_pay = true; }
         if (typeof WeixinJSBridge == "undefined") {
             if (document.addEventListener) {
@@ -16,64 +15,96 @@
                 document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
             }
         } else { onBridgeReady(); }
-
-
-        if ($.isEmptyObject(openid))
-            alert('OpenID错误');
-        else {
-            if ($.fn.wxPay.WxVersion() < "5.0") {
-                alert("请升级微信客户端");
-                return;
+        var yuan2fen=function (r) {
+            var m = 0, s1 = r.toString(), s2 = "100";
+            try { m += s1.split(".")[1].length } catch (e) { }
+            try { m += s2.split(".")[1].length } catch (e) { }
+            return Number(s1.replace(".", "")) * Number(s2.replace(".", "")) / Math.pow(10, m);
+        };
+        var compare=function(curV,reqV){
+            if(curV && reqV){
+               var arr1 = curV.split('.'),
+                   arr2 = reqV.split('.');
+               var minLength=Math.min(arr1.length,arr2.length),
+                   position=0,
+                   diff=0;
+               while(position<minLength && ((diff=parseInt(arr1[position])-parseInt(arr2[position]))==0)){
+                   position++;
+               }
+               diff=(diff!=0)?diff:(arr1.length-arr2.length);
+               return diff>0;
+            }else{
+               return false;
             }
-            if (settings.desc == "") settings.desc = "备注不能为空-wxpay.net";
-            $(this).click(function () {
-                abortpay = false;
-                if (!wx_pay && !$.fn.wxPay.Debug) { return; } //alert("正在初始化支付控件或为非微信内置浏览器环境，请稍候再试！");
-                var fee = 0;
-                if ($.isFunction(paybefore)) {
-                    var rfee = paybefore();
-                    if ($.isEmptyObject(rfee) || !$.isNumeric(rfee)) abortpay = true; //中止支付                        
-                    if (!isNaN(parseFloat(rfee)))
-                        fee = (function () {
-                            var m = 0, s1 = rfee.toString(), s2 = "100";
-                            try { m += s1.split(".")[1].length } catch (e) { }
-                            try { m += s2.split(".")[1].length } catch (e) { }
-                            return Number(s1.replace(".", "")) * Number(s2.replace(".", "")) / Math.pow(10, m);
-                        })().toFixed(0); //转换成分
-                }
-                if (!abortpay && fee > 0) {
-                    $.post(signurl, { openid: openid, tfee: fee, body: settings.desc, pid: settings.pid, param: $.fn.wxPay.OrderParam, sp_billno: $.fn.wxPay.OrderCode }, function (data) {
-                        if (data.paySign == "ERROR") {
-                            if ($.isFunction(fail)) fail(data.package);
-                        }
-                        else {
-                            WeixinJSBridge.invoke(
-                            'getBrandWCPayRequest', {
-                                "appId": data.appId, //公众号名称，由商户传入
-                                "timeStamp": data.timeStamp, //时间戳
-                                "nonceStr": data.nonceStr, //随机串
-                                "package": data.package,//扩展包
-                                "signType": "MD5", //微信签名方式
-                                "paySign": data.paySign //微信签名
-                            }, function (res) {
-                                if (res.err_msg == "get_brand_wcpay_request:ok") {
-                                    if ($.isFunction(success)) success();
-                                } else
-                                    if (res.err_msg == "get_brand_wcpay_request:fail") {
-                                        if ($.isFunction(fail)) fail(res.err_desc);
-                                    } else {
-                                        if ($.isFunction(cancel)) cancel(data.payNo);
-                                    }
-                            });
-                        }
-                    }, "json");
-                }
+        };
+        if ($.isEmptyObject(openid)&&!$.fn.wxPay.Debug){
+            $(this).click(function(){
+                alert('用户信息错误，无法调用微信支付');
             });
+            $(this).click();
+        }else {
+            var ver=$.fn.wxPay.WxVersion();
+            if (!compare(ver,"8.0.3")&&!$.fn.wxPay.Debug) {
+                $(this).click(function(){
+                    alert("请安装并升级微信APP，版本>=8.0.3，当前："+(ver=="0"?"非微信浏览器":ver));
+                });
+                $(this).click();
+            }else{
+                if (settings.desc == "") settings.desc = "备注不能为空";else
+                {
+                    var canRun = true;                    
+                    $(this).click(function () {
+                        if(canRun){
+                            canRun = false;
+                            if (!wx_pay && !$.fn.wxPay.Debug) {canRun = true; return; }
+                            var fee = 0;
+                            if (typeof paybefore === "function") {
+                                var rfee = paybefore();                        
+                                if(isNaN(rfee)){canRun = true;return;} //如果返回的金额是NaN，取消支付流程                                                        
+                                if (parseFloat(rfee)>0)
+                                    fee = yuan2fen(rfee).toFixed(0); //转换成分
+                                else
+                                    fee=rfee;
+                            }
+                            if(typeof paybefore === 'number' && !isNaN(paybefore))
+                                fee=yuan2fen(paybefore).toFixed(0);
+                            if (!(fee==0)) { //如果金额小于0，让后台决定金额，应用场景可以是例如固定或有规则金额的情况下
+                                $.post(signurl, { openid: openid, tfee: fee, body: settings.desc, pid: settings.pid, param: $.fn.wxPay.OrderParam, sp_billno: $.fn.wxPay.OrderCode }, function (data) {
+                                    if (data.paySign == "ERROR") {
+                                        if (typeof fail === "function") fail(data.package);
+                                    }
+                                    else {
+                                        WeixinJSBridge.invoke(
+                                        'getBrandWCPayRequest', {
+                                            "appId": data.appId, //公众号名称，由商户传入
+                                            "timeStamp": data.timeStamp, //时间戳
+                                            "nonceStr": data.nonceStr, //随机串
+                                            "package": data.package,//扩展包
+                                            "signType": "MD5", //微信签名方式
+                                            "paySign": data.paySign //微信签名
+                                        }, function (res) {
+                                            if (res.err_msg == "get_brand_wcpay_request:ok") {
+                                                if (typeof success === "function") success({fee:data.fee,ordercode:data.ordercode});
+                                            } else
+                                                if (res.err_msg == "get_brand_wcpay_request:fail") {
+                                                    if (typeof fail === "function") fail(res.err_desc);
+                                                } else {
+                                                    if (typeof cancel === "function") cancel(data.payNo);
+                                                }
+                                        });
+                                    }
+                                }, "json");
+                            }                            
+                            setTimeout(function(){canRun = true;}, 800);
+                        }
+                    });                    
+                }
+            }
         }
     };
     $.fn.SelectAddress = function (appid, addsign, timestamp, noncestr, success, fail, cancel) {
-        if ($.fn.wxPay.WxVersion() < "5.0") {
-            alert("请升级微信客户端");
+        if ($.fn.wxPay.WxVersion() < 5) {
+            alert("请使用或升级微信客户端");
         } else {
             $(this).click(function () {
                 WeixinJSBridge.invoke("editAddress",
@@ -104,12 +135,12 @@
     };
     $.fn.wxPay.SelectedAddr = "";
     $.fn.wxPay.OrderParam = "";
-    $.fn.wxPay.OrderCode = "";
+    $.fn.wxPay.OrderCode = ""; //为空，让系统生成并返回
     $.fn.wxPay.Debug = false;
     $.fn.wxPay.WxVersion = function () {
         var weInfo = navigator.userAgent.match(/MicroMessenger\/([\d\.]+)/i);
         if (!weInfo)
-            return 0;
+            return "0";
         else
             return weInfo[1];
     };
